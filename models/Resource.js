b4b8c824ac2ff90d967ee0a70cb1527c53ce5500
4theKnowledge <tylerbikaun@gmail.com>
2022-08-31 16:21:21 +0800

    changed semantics for maps.

---
+++ models/Resource.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const ResourceSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+    },
+    tokens: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+    replacements: [{ type: mongoose.Schema.Types.Mixed, required: false }],
+    color: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  },
+  { _id: true, timestamps: true }
+);
+
+module.exports = mongoose.model("Resource", ResourceSchema);
