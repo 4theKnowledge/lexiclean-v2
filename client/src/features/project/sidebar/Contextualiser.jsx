cdc5840bfbfc2d79398a3d521a8b553fc5abd5e1
4theKnowledge <tylerbikaun@gmail.com>
2024-02-15 08:05:29 +0800

    Updated state variables. Styled.

--- client/src/features/project/sidebar/Contextualiser.jsx
+++ client/src/features/project/sidebar/Contextualiser.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Typography, Grid, Stack, Chip } from "@mui/material";
+import { Typography, Grid, Stack, Chip, Box, Paper } from "@mui/material";
 import axios from "axios";
 import { ProjectContext } from "../../../shared/context/project-context";
 
@@ -11,45 +11,60 @@ const Contextualiser = () => {
     const fetchData = async () => {
       const response = await axios.post("/api/token/search", {
         projectId: state.projectId,
-        value: state.selectedTokenValue,
+        value: state.selectedToken.value,
       });
 
       setData(response.data);
     };
 
-    if (state.selectedTokenValue) {
+    if (state.selectedToken && state.selectedToken.value) {
       fetchData();
     }
-  }, [state.selectedTokenValue]);
+  }, [state.selectedToken]);
 
-  if (data) {
-    return (
-      <Grid p={2}>
-        <Typography variant="caption">
-          Replacements made on {state.selectedTokenValue}
-        </Typography>
-        <Stack direction="row" spacing={2} mb={2} pt={1}>
-          {Object.keys(data.replacements).length === 0 ? (
-            <Chip label={"No replacements found"} size="small" />
-          ) : (
-            Object.keys(data.replacements).map((value) => (
-              <Chip
-                key={`replacement-${value}-${data.replacements[value]}`}
-                label={`${value}: ${data.replacements[value]}`}
-                size="small"
-              />
-            ))
-          )}
-        </Stack>
-        {/* <Typography variant="caption">Similar Terms</Typography>
-        <Stack direction="row" spacing={2} p={1}>
-          {data.similar.map((item) => (
-            <Chip key={`similar-${item}`} label={item} size="small" />
-          ))}
-        </Stack> */}
-      </Grid>
-    );
-  }
+  return (
+    <Paper sx={{ backgroundColor: "background.default" }} elevation={0}>
+      <Box
+        display="flex"
+        p={2}
+        flexDirection="column"
+        sx={{ whiteSpace: "normal", overflowWrap: "break-word" }}
+      >
+        {state.selectedToken ? (
+          <Typography>Selected: {state.selectedToken.value}</Typography>
+        ) : (
+          <Typography>Nothing Selected</Typography>
+        )}
+        {state.selectedToken && data && (
+          <>
+            <Typography variant="caption">
+              Replacements made on tokens of the same value:
+            </Typography>
+            <Stack direction="row" spacing={2} pt={1}>
+              {Object.keys(data.replacements).length === 0 ? (
+                <Chip label={"Nothing found"} size="small" />
+              ) : (
+                Object.keys(data.replacements).map((value) => (
+                  <Chip
+                    color="primary"
+                    key={`replacement-${value}-${data.replacements[value]}`}
+                    label={`${value}: ${data.replacements[value]}`}
+                    size="small"
+                  />
+                ))
+              )}
+            </Stack>
+            {/* <Typography variant="caption">Similar Terms</Typography>
+            <Stack direction="row" spacing={2} p={1}>
+              {data.similar.map((item) => (
+                <Chip key={`similar-${item}`} label={item} size="small" />
+              ))}
+            </Stack> */}
+          </>
+        )}
+      </Box>
+    </Paper>
+  );
 };
 
 export default Contextualiser;
