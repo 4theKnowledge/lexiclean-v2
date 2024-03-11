f5577293486e62558a93432dad95546d62e9dc96
4theKnowledge <tylerbikaun@gmail.com>
2024-03-11 16:50:08 +0800

    added auth0 provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-import { ProjectProvider } from "./shared/context/project-context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <ProjectProvider>
+  <Auth0Provider
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: `https://${process.env.REACT_APP_AUTH0_AUDIENCE}`,
+    }}
+    cacheLocation={"localstorage"}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    domain={`https://${process.env.REACT_APP_AUTH0_DOMAIN}`}
+    useRefreshTokens={true}
+    useRefreshTokensFallback={true}
+  >
     <App />
-  </ProjectProvider>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
