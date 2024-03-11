cb0d1f1c21248a98b06d8b0d27cdbae07a5aed14
4theKnowledge <tylerbikaun@gmail.com>
2024-03-11 13:30:45 +0800

    added route for fetching db user details

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,17 +2,45 @@ const express = require("express");
 const router = express.Router();
 const logger = require("../logger");
 const User = require("../models/User");
+const { jwtDecode } = require("jwt-decode");
+
+router.get("/", async (req, res) => {
+  const user = jwtDecode(req.headers.authorization.replace("Bearer ", ""));
+
+  try {
+    // Check if user exists in db
+    const userResponse = await User.findOne({ auth0Id: user.sub }).lean();
+
+    if (!userResponse) {
+      // Create user and return
+      newUser = new User({
+        username: user.nickname,
+        email: user.email,
+        name: user.nickname,
+        auth0Id: user.sub,
+      });
+      await newUser.save();
+    }
+    res.json(userResponse);
+  } catch (error) {
+    console.error("Error accessing user data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 router.get("/system", async (req, res) => {
   // Fetch system user which is used in the single annotator application variant.
+  // TODO: Review this when the system's build config is put together.
   const user = await User.findOne({ username: "system" }).lean();
   res.json(user);
 });
 
 router.patch("/:userId", async (req, res) => {
   try {
-    // Extract userID from request parameters
-    const userId = req.params.userId;
+    // Extract userID from request parameters (this is the Auth0 `sub`)
+    const userId = req.params.sub;
+
+    console.log(`userId: ${userId}`);
 
     // Find the user by ID
     const user = await User.findById(userId);
