ed0d975d2083c32ddf3fee0260c26fa106fa85e4
4theKnowledge <tylerbikaun@gmail.com>
2024-03-11 16:49:59 +0800

    added extra util

--- client/src/shared/utils/general.js
+++ client/src/shared/utils/general.js
@@ -1,5 +1,5 @@
 export const truncateText = (text, length) => {
-  return text.length > length ? text.sice(0, length) + "..." : text;
+  return text.length > length ? text.slice(0, length) + "..." : text;
 };
 
 export const camelCaseToStandardEnglish = (inputString) => {
@@ -11,4 +11,33 @@ export const camelCaseToStandardEnglish = (inputString) => {
     .trim();
 
   return result;
+};
+
+export const getMostRecentDate = (dates) => {
+  /**
+   * Takes an array of UTC date strings in the format "yyyy-mm-ddThh:mm:ss.ssssss" and returns the most recent date as a UTC Date object.
+   * @param {Array} dates - An array of UTC date strings.
+   * @returns {Date} The most recent date in UTC format.
+   */
+  // Convert each date string to a Date object and store in an array
+  const dateObjects = dates.map((dateString) => new Date(dateString));
+
+  // Use the reduce() method to find the maximum Date object in the array
+  const maxDate = dateObjects.reduce(
+    (max, date) => (date > max ? date : max),
+    new Date(0)
+  );
+
+  // Return the maximum date as a UTC Date object
+  return new Date(
+    Date.UTC(
+      maxDate.getFullYear(),
+      maxDate.getMonth(),
+      maxDate.getDate(),
+      maxDate.getHours(),
+      maxDate.getMinutes(),
+      maxDate.getSeconds(),
+      maxDate.getMilliseconds()
+    )
+  );
 };
