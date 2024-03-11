78f601fc622c0fec2602174ee2753926d12ce3ce
4theKnowledge <tylerbikaun@gmail.com>
2024-03-11 16:51:03 +0800

    added auth0 logic

--- client/src/shared/context/AppContext.js
+++ client/src/shared/context/AppContext.js
@@ -1,13 +1,14 @@
 // This context will manage the user, their preferences, notifications, etc.
-
 import React, { createContext, useContext, useEffect, useReducer } from "react";
 import axiosInstance from "../api/axiosInstance";
+import { useAuth0 } from "@auth0/auth0-react";
 
 // Initial state
 const initialState = {
   user: null, // User account information
   notifications: [], // User notifications
   preferences: {}, // User preferences
+  token: null,
 };
 
 // Create context
@@ -20,6 +21,8 @@ function appReducer(state, action) {
       return { ...state, user: action.payload };
     case "LOGOUT_USER":
       return { ...initialState };
+    case "SET_TOKEN":
+      return { ...state, token: action.payload };
     case "SET_NOTIFICATIONS":
       return { ...state, notifications: action.payload };
     case "SET_PREFERENCES":
@@ -32,23 +35,45 @@ function appReducer(state, action) {
 // Context provider component
 export function AppProvider({ children }) {
   const [state, dispatch] = useReducer(appReducer, initialState);
+  const {
+    isAuthenticated,
+    user: auth0User,
+    getAccessTokenSilently,
+  } = useAuth0();
 
   useEffect(() => {
-    async function fetchUserData() {
-      try {
-        const response = await axiosInstance.get("/api/user/system");
-        if (response.status !== 200) {
-          throw new Error("Failed to fetch user data");
-        }
-        const userData = await response.data;
-        dispatch({ type: "SET_USER", payload: userData });
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    }
+    if (isAuthenticated && auth0User) {
+      // Fetch additional user data based on Auth0 user information
+      async function fetchUserData() {
+        try {
+          const auth0Token = await getAccessTokenSilently({
+            authorizationParams: {
+              audience: `https://${process.env.REACT_APP_AUTH0_AUDIENCE}`,
+            },
+          });
+          dispatch({ type: "SET_TOKEN", payload: auth0Token });
 
-    fetchUserData();
-  }, []); // Empty dependency array ensures this runs once on mount
+          const response = await axiosInstance.get("/api/user/", {
+            headers: {
+              Authorization: `Bearer ${auth0Token}`,
+            },
+          });
+
+          if (response.status !== 200) {
+            throw new Error("Failed to fetch user data");
+          }
+          const userData = await response.data;
+          dispatch({ type: "SET_USER", payload: userData });
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      }
+      fetchUserData();
+    } else {
+      // Handle logout
+      dispatch({ type: "LOGOUT_USER" });
+    }
+  }, [isAuthenticated, auth0User]); // Depend on Auth0's authentication state.
 
   // Value to be passed to the provider
   const value = { state, dispatch };
