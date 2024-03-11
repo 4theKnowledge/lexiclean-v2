3923647caf018f8e2b04185757e6aaf5fab3c16f
4theKnowledge <tylerbikaun@gmail.com>
2024-03-11 08:32:01 +0800

    updated logic and restyled

--- client/src/features/projectcreation/steps/Preprocessing.jsx
+++ client/src/features/projectcreation/steps/Preprocessing.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import {
   Grid,
   TextField,
@@ -10,96 +10,103 @@ import {
   FormControl,
   Checkbox,
   Paper,
+  Skeleton,
+  Box,
 } from "@mui/material";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
-const Preprocessing = (props) => {
-  const { values, updateValue } = props;
-  const corpus = Object.values(values["corpus"]);
+const Preprocessing = ({ values, updateValue }) => {
+  const corpus = useMemo(() => Object.values(values["corpus"]), [values]);
   const [originalCorpusDetails, setOriginalCorpusDetails] = useState({});
   const [previewContent, setPreviewContent] = useState(
     "Upload texts to preview"
   );
-  const [corpusDetails, setCorpusDetails] = useState({});
-
-  const [corpusSize, setCorpusSize] = useState();
-  const [vocabSize, setVocabSize] = useState();
-  const [tokenSize, setTokenSize] = useState();
+  const [corpusDetails, setCorpusDetails] = useState({
+    corpusSize: 0,
+    vocabSize: 0,
+    tokenSize: 0,
+  });
 
   useEffect(() => {
-    // Update preview data whenever a text file is uploaded and the pre-processing
-    // actions are changed
-    if (Object.keys(originalCorpusDetails).length === 0) {
-      // Add original corpus details for user
-      // Remove multiple white space and trim
-      const originalCorpus = corpus.map((text) =>
-        text.replace(/\s+/g, " ").trim()
-      );
-      setOriginalCorpusDetails({
-        corpusSize: originalCorpus.length,
-        vocabSize: new Set(originalCorpus.map((text) => text.split(" ")).flat())
-          .size,
-        tokenSize: originalCorpus.map((text) => text.split(" ")).flat().length,
-      });
-    }
+    /**
+     * Update preview data whenever a text file is uploaded and the pre-processing
+     * actions are changed.
+     */
+    const processCorpus = () => {
+      if (
+        corpus.length !== 0 &&
+        Object.keys(originalCorpusDetails).length === 0
+      ) {
+        /**
+         * 1. Add original corpus details for user
+         * 2. Remove multiple white space and trim
+         */
 
-    if (corpus) {
-      // Remove multiple white space and trim
-      let preCorpus = corpus.map((text) => text.replace(/\s+/g, " ").trim());
-
-      if (values["preprocessLowerCase"]) {
-        preCorpus = preCorpus.map((text) => text.toLowerCase());
+        const originalCorpus = corpus.map((text) =>
+          text.replace(/\s+/g, " ").trim()
+        );
+        setOriginalCorpusDetails({
+          corpusSize: originalCorpus.length,
+          vocabSize: new Set(
+            originalCorpus.map((text) => text.split(" ")).flat()
+          ).size,
+          tokenSize: originalCorpus.map((text) => text.split(" ")).flat()
+            .length,
+        });
       }
-      if (values["preprocessRemoveDuplicates"]) {
-        preCorpus = [...new Set(preCorpus)];
-      }
-      if (values["preprocessRemoveChars"]) {
-        const escapedChars = [
-          "[",
-          "]",
-          "{",
-          "}",
-          "(",
-          ")",
-          "*",
-          "+",
-          "?",
-          "|",
-          "^",
-          "$",
-          ".",
-          "\\",
-        ];
 
-        const regexCharsEscaped = values["preprocessRemoveCharSet"]
-          .split("")
-          .map((char) => (escapedChars.includes(char) ? `\\${char}` : char));
-        const regex = new RegExp("[" + regexCharsEscaped + "]", "g");
-        preCorpus = preCorpus.map((text) => text.replace(regex, " "));
+      if (corpus) {
         // Remove multiple white space and trim
-        preCorpus = preCorpus.map((text) => text.replace(/\s+/g, " ").trim());
+        let preCorpus = corpus.map((text) => text.replace(/\s+/g, " ").trim());
+
+        if (values["preprocessLowerCase"]) {
+          preCorpus = preCorpus.map((text) => text.toLowerCase());
+        }
+        if (values["preprocessRemoveDuplicates"]) {
+          preCorpus = [...new Set(preCorpus)];
+        }
+        if (values["preprocessRemoveChars"]) {
+          const escapedChars = [
+            "[",
+            "]",
+            "{",
+            "}",
+            "(",
+            ")",
+            "*",
+            "+",
+            "?",
+            "|",
+            "^",
+            "$",
+            ".",
+            "\\",
+          ];
+
+          const regexCharsEscaped = values["preprocessRemoveCharSet"]
+            .split("")
+            .map((char) => (escapedChars.includes(char) ? `\\${char}` : char));
+          const regex = new RegExp("[" + regexCharsEscaped + "]", "g");
+          preCorpus = preCorpus.map((text) => text.replace(regex, " "));
+          // Remove multiple white space and trim
+          preCorpus = preCorpus.map((text) => text.replace(/\s+/g, " ").trim());
+        }
+
+        // Add data uploaded to preview content
+        setPreviewContent(preCorpus.slice(0, 1000).join("\n"));
+
+        setCorpusDetails({
+          corpusSize: preCorpus.length,
+          vocabSize: new Set(preCorpus.map((text) => text.split(" ")).flat())
+            .size,
+          tokenSize: preCorpus.map((text) => text.split(" ")).flat().length,
+        });
       }
-
-      // Add data uploaded to preview content
-      setPreviewContent(preCorpus.slice(0, 1000).join("\n"));
-
-      setCorpusDetails({
-        corpusSize: preCorpus.length,
-        vocabSize: new Set(preCorpus.map((text) => text.split(" ")).flat())
-          .size,
-        tokenSize: preCorpus.map((text) => text.split(" ")).flat().length,
-      });
-    }
+    };
+    processCorpus();
   }, [corpus, values]);
-
-  useEffect(() => {
-    if (corpus && corpus[0] === "") {
-      // Reset preview content
-      setPreviewContent("Upload texts to preview");
-    }
-  }, [corpus]);
 
   const actionAppliedToCorpus =
     values["preprocessLowerCase"] ||
@@ -118,80 +125,25 @@ const Preprocessing = (props) => {
         variant="outlined"
         mb={2}
       >
-        {Object.keys(corpusDetails).map((key) => {
-          return (
-            <Grid item xs={3} p={2} key={`grid-item-${key}`}>
-              <Stack direction="column" alignItems="center">
-                {actionAppliedToCorpus ? (
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "center",
-                      justifyContent: "space-evenly",
-                    }}
-                  >
-                    <Stack direction="row" alignItems="center">
-                      <span style={{ color: "#90a4ae" }}>
-                        {originalCorpusDetails[key].toLocaleString()}
-                      </span>
-                      <ArrowRightAltIcon
-                        sx={{
-                          margin: "0rem 0.25rem",
-                          fontSize: "1rem",
-                        }}
-                      />
-                      <span>{corpusDetails[key].toLocaleString()}</span>
-                      {originalCorpusDetails[key] !== corpusDetails[key] && (
-                        <Stack
-                          direction="row"
-                          alignItems="center"
-                          sx={{
-                            fontSize: "0.75rem",
-                            color:
-                              originalCorpusDetails[key] > corpusDetails[key]
-                                ? "#2e7d32"
-                                : "#c62828",
-                          }}
-                        >
-                          {originalCorpusDetails[key] > corpusDetails[key] ? (
-                            <ArrowDownwardIcon sx={{ fontSize: "1rem" }} />
-                          ) : (
-                            <ArrowUpwardIcon sx={{ fontSize: "1rem" }} />
-                          )}
-                          {Math.round(
-                            Math.abs(
-                              ((originalCorpusDetails[key] -
-                                corpusDetails[key]) *
-                                100) /
-                                originalCorpusDetails[key]
-                            )
-                          )}
-                          %
-                        </Stack>
-                      )}
-                    </Stack>
-                    {/* This operator doesnt seem to avoid showing up/down when original equals processed... TODO */}
-                  </div>
-                ) : (
-                  <span style={{ fontWeight: "bold" }}>
-                    {corpusDetails[key].toLocaleString()}
-                  </span>
-                )}
-                <span
-                  id="section-subtitle"
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    textTransform: "capitalize",
-                  }}
-                >
-                  {key.replace("Size", "")} Size
-                </span>
-              </Stack>
-            </Grid>
-          );
-        })}
+        {Object.keys(corpusDetails).map((key) => (
+          <Grid item xs={3} p={2} key={`grid-item-${key}`}>
+            <Stack direction="column" alignItems="center">
+              <MetricWithDiff
+                actionAppliedToCorpus={actionAppliedToCorpus}
+                originalCorpusDetails={originalCorpusDetails}
+                corpusDetails={corpusDetails}
+                name={key}
+              />
+              <Typography
+                id="section-subtitle"
+                key={`detail-subtitle-${key}`}
+                sx={{ textTransform: "capitalize" }}
+              >
+                {key.replace("Size", "")} Size
+              </Typography>
+            </Stack>
+          </Grid>
+        ))}
       </Grid>
       <Grid container item xs={12}>
         <Grid item xs={12} component={Paper} variant="outlined" mb={2}>
@@ -276,6 +228,80 @@ const Preprocessing = (props) => {
       </Grid>
     </Grid>
   );
+};
+
+const iconStyle = {
+  margin: "0rem 0.25rem",
+  fontSize: "1rem",
+};
+
+const MetricWithDiff = ({
+  actionAppliedToCorpus,
+  originalCorpusDetails,
+  corpusDetails,
+  name,
+}) => {
+  const comparisonResult = useMemo(() => {
+    const originalValue = originalCorpusDetails[name];
+    const newValue = corpusDetails[name];
+    const difference = Math.abs(
+      ((originalValue - newValue) * 100) / originalValue
+    );
+    const isDecreased = originalValue > newValue;
+
+    return {
+      isChanged: originalValue !== newValue,
+      color: isDecreased ? "#2e7d32" : "#c62828",
+      icon: isDecreased ? (
+        <ArrowDownwardIcon sx={iconStyle} />
+      ) : (
+        <ArrowUpwardIcon sx={iconStyle} />
+      ),
+      difference: Math.round(difference),
+    };
+  }, [originalCorpusDetails[name], corpusDetails[name]]);
+
+  if (!name) {
+    return <Skeleton width={30} />;
+  }
+
+  if (actionAppliedToCorpus) {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="space-evenly"
+      >
+        <Stack direction="row" alignItems="center">
+          <Typography color="text.secondary">
+            {originalCorpusDetails[name].toLocaleString()}
+          </Typography>
+          <ArrowRightAltIcon sx={iconStyle} />
+          <Typography>{corpusDetails[name].toLocaleString()}</Typography>
+          {comparisonResult.isChanged && (
+            <Stack
+              direction="row"
+              alignItems="center"
+              sx={{
+                fontSize: "0.75rem",
+                color: comparisonResult.color,
+              }}
+            >
+              {comparisonResult.icon}
+              {comparisonResult.difference}%
+            </Stack>
+          )}
+        </Stack>
+      </Box>
+    );
+  } else {
+    return (
+      <Typography fontWeight="bold">
+        {corpusDetails[name].toLocaleString()}
+      </Typography>
+    );
+  }
 };
 
 export default Preprocessing;
