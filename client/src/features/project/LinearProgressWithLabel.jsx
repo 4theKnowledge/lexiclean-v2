2d85cf67e2c4a802eb101e322446a102e221565d
4theKnowledge <tylerbikaun@gmail.com>
2024-03-11 16:47:50 +0800

    updated api to hook. added confetti animation

--- client/src/features/project/LinearProgressWithLabel.jsx
+++ client/src/features/project/LinearProgressWithLabel.jsx
@@ -1,32 +1,42 @@
-import { useEffect, useState, useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { LinearProgress, Box, Tooltip } from "@mui/material";
-import { ProjectContext } from "../../shared/context/project-context";
+import { ProjectContext } from "../../shared/context/ProjectContext";
+import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
+import { ANNOTATION_SIDEBAR_WIDTH } from "../../shared/constants/layout";
 
 const LinearProgressWithLabel = () => {
-  const [{ progress }, dispatch] = useContext(ProjectContext);
-  const [progressState, setProgressState] = useState({ value: 0, text: "" });
+  const [{ progress }] = useContext(ProjectContext);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const { width, height } = useWindowSize();
 
   useEffect(() => {
-    if (progress) {
-      const { value, title } = progress;
-      setProgressState({
-        value: value,
-        text: title,
-      });
+    // Show confetti when progress.value reaches 100 (100%)
+    if (progress && progress.value === 100) {
+      setShowConfetti(true);
+      // Optionally, stop the confetti after a certain time
+      const timer = setTimeout(() => setShowConfetti(false), 5000); // Stop after 5 seconds
+      return () => clearTimeout(timer);
     }
-  }, [progress]);
+  }, [progress.value]);
 
   return (
     <Tooltip
-      title={`Current annotation progress: ${progressState.text}`}
+      title={`Current annotation progress: ${progress.title}`}
       placement="top"
     >
-      <Box sx={{ width: "100%", cursor: "help" }}>
+      <Box sx={{ width: "100%", cursor: "help", position: "relative" }}>
         <LinearProgress
           variant="determinate"
-          value={progressState.value}
+          value={progress.value}
           sx={{ height: "6px" }}
         />
+        {showConfetti && (
+          <Confetti
+            width={width - ANNOTATION_SIDEBAR_WIDTH - 8}
+            height={height - 64}
+          />
+        )}
       </Box>
     </Tooltip>
   );
