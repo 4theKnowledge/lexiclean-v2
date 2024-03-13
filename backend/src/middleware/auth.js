e192052cc569494f4144b95ee87e32fc6bbe2110
4theKnowledge <tylerbikaun@gmail.com>
2024-03-13 21:01:57 +0800

    removed comments. updated semantics. added middleware for project management validation.

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -16,8 +16,10 @@ export const authenticateUser = async (req, res, next) => {
 
 export const projectAccessCheck = async (req, res, next) => {
   try {
-    const userId = req.userId; // Assuming userId is already attached to req
-    const projectId = req.params.projectId || req.body.projectId; // Adjust based on how projectId is passed
+    const userId = req.userId;
+    const projectId = req.params.projectId || req.body.projectId;
+
+    console.log(userId, projectId);
 
     if (!projectId) {
       return res.status(400).json({ message: "Project ID is required." });
@@ -29,12 +31,44 @@ export const projectAccessCheck = async (req, res, next) => {
       return res.status(404).json({ message: "Project not found." });
     }
 
-    const isUser = project.user.toString() === userId;
+    const isOwner = project.owner.toString() === userId.toString();
     const isAnnotator = project.annotators
       .map((a) => a.toString())
-      .includes(userId);
+      .includes(userId.toString());
 
-    if (!isUser && !isAnnotator) {
+    if (!isOwner && !isAnnotator) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized access to project." });
+    }
+
+    next(); // User has access, proceed to the next middleware/route handler
+  } catch (error) {
+    console.error("Error checking project access:", error);
+    return res.status(500).json({ message: "Failed to check project access." });
+  }
+};
+
+/**
+ * Checks whether user is owner and has privildged rights to manage project.
+ */
+export const projectManagementCheck = async (req, res, next) => {
+  try {
+    const userId = req.userId;
+    const projectId = req.params.projectId || req.body.projectId;
+
+    if (!projectId) {
+      return res.status(400).json({ message: "Project ID is required." });
+    }
+
+    const project = await Project.findById(projectId).lean();
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found." });
+    }
+
+    const isOwner = project.owner.toString() === userId.toString();
+    if (!isOwner) {
       return res
         .status(403)
         .json({ message: "Unauthorized access to project." });
