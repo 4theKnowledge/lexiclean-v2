5bc28bec1556f9406bd2b3151dc1c91ed548b5d6
4theKnowledge <tylerbikaun@gmail.com>
2024-03-13 21:35:00 +0800

    styled delete project

--- client/src/features/Dashboard/Settings.jsx
+++ client/src/features/Dashboard/Settings.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import StyledCard from "./StyledCard";
 
@@ -30,35 +30,39 @@ const Settings = ({ loading, data, downloadProject, deleteProject }) => {
             Download
           </Button>
         </Box>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Box>
-            <Typography fontWeight="bold" color="text.secondary">
-              Delete Project
-            </Typography>
-            <Typography variant="caption">
-              Enter the projects name and click 'delete' to permanently remove
-              this project. This is irreversible.
-            </Typography>
-          </Box>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <TextField
-              placeholder={`Enter project name (${data.details.name})`}
-              onChange={(e) => setDeleteName(e.target.value)}
-              value={deleteName}
-              size="small"
-              color="error"
-              fullWidth
-            />
-            <Button
-              variant="contained"
-              onClick={deleteProject}
-              disabled={loading || data.details.name !== deleteName}
-              color="error"
-            >
-              Delete
-            </Button>
-          </Stack>
-        </Box>
+        <Grid container alignItem="center">
+          <Grid item xs={7}>
+            <Box>
+              <Typography fontWeight="bold" color="text.secondary">
+                Delete Project
+              </Typography>
+              <Typography variant="caption">
+                Enter the projects name and click 'delete' to permanently remove
+                this project. This is irreversible.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={5}>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <TextField
+                placeholder={`Enter project name (${data.details.name})`}
+                onChange={(e) => setDeleteName(e.target.value)}
+                value={deleteName}
+                size="small"
+                error
+                fullWidth
+              />
+              <Button
+                variant="contained"
+                onClick={deleteProject}
+                disabled={loading || data.details.name !== deleteName}
+                color="error"
+              >
+                Delete
+              </Button>
+            </Stack>
+          </Grid>
+        </Grid>
       </Box>
     </StyledCard>
   );
