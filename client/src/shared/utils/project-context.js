a013f233f83d9504d8e6fb224fc96a81284fa1b0
4theKnowledge <tylerbikaun@gmail.com>
2024-03-13 10:34:48 +0800

    added logic for handling token tag updates.

--- client/src/shared/utils/project-context.js
+++ client/src/shared/utils/project-context.js
@@ -63,3 +63,55 @@ export const updateTexts = (
 
   return updatedTexts;
 };
+
+export const updateTextTokenTags = ({
+  action,
+  texts,
+  textTokenIds,
+  focusTokenId,
+  entityLabelId,
+}) => {
+  /**
+   * Performs operations on a set of texts comprised of token(s) such as apply, accept, or delete.
+   * `textTokenIds` is {textId: [tokenId, ..., tokenId]}`
+   */
+
+  let updatedTexts = texts;
+
+  Object.keys(textTokenIds).map((textId) => {
+    const tokenIdsToUpdate = textTokenIds[textId];
+
+    const text = texts[textId];
+    const newTokens = Object.values(text.tokens).map((token) => {
+      if (tokenIdsToUpdate.includes(token._id)) {
+        switch (action) {
+          case "apply":
+            return {
+              ...token,
+              tags: [...token.tags, entityLabelId],
+            };
+          case "delete":
+            return {
+              ...token,
+              tags: token.tags.filter((t) => t !== entityLabelId),
+            };
+          // case "accept":
+          //   return {
+          //     ...token,
+          //   };
+          default:
+            throw new Error("Token operation/action not specified correctly");
+        }
+      } else {
+        return token;
+      }
+    });
+
+    updatedTexts = {
+      ...updatedTexts,
+      [textId]: { ...text, tokens: newTokens },
+    };
+  });
+
+  return updatedTexts;
+};
