09285861354150f0a85fc255a6dea3910a2bd085
4theKnowledge <tylerbikaun@gmail.com>
2024-03-13 21:08:08 +0800

    added basic styling for mocking.

--- client/src/features/Dashboard/Annotators.jsx
+++ client/src/features/Dashboard/Annotators.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import StyledCard from "./StyledCard";
-import { Alert, AlertTitle, Box } from "@mui/material";
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const Annotators = () => {
   return (
@@ -8,16 +17,50 @@ const Annotators = () => {
       <Box p="0rem 1rem">
         <Alert severity="info">
           <AlertTitle>Project Annotators</AlertTitle>
-          This area allows you invite annotators to your project.
+          This area allows you invite annotators to your project. Annotators
+          will have access to all texts associated with the project.
         </Alert>
       </Box>
-      <InviteTable />
+      <Box p={2}>
+        <Typography>Coming soon!</Typography>
+      </Box>
+      {/* <InviteForm /> */}
+      {/* <AnnotatorTable /> */}
     </StyledCard>
   );
 };
 
-const InviteTable = () => {
-  return <Box>Hello world</Box>;
+const InviteForm = () => {
+  return (
+    <Box p={2}>
+      <Paper sx={{ p: 2 }} variant="outlined">
+        <Typography variant="body1" gutterBottom>
+          Invite project annotators
+        </Typography>
+        <Stack spacing={1} direction="row">
+          <TextField fullWidth placeholder="Enter comma-spaced usernames" />
+          <Button>Invite</Button>
+        </Stack>
+      </Paper>
+    </Box>
+  );
+};
+
+const AnnotatorTable = () => {
+  const data = [{ username: "dummy 1" }, { username: "dummy 2" }];
+
+  return (
+    <Box p={2}>
+      <Paper sx={{ p: 2 }} variant="outlined">
+        <Typography variant="body1">Active Project Annotators</Typography>
+        <ul>
+          {data.map((a) => (
+            <li>{a.username}</li>
+          ))}
+        </ul>
+      </Paper>
+    </Box>
+  );
 };
 
 export default Annotators;
