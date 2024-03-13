22142569ba5217e538178246567e9fe53cb99262
4theKnowledge <tylerbikaun@gmail.com>
2024-03-13 10:36:09 +0800

    added more functionality.

--- client/src/features/project/TagPopover.jsx
+++ client/src/features/project/TagPopover.jsx
@@ -1,19 +1,19 @@
 import { IconButton, Stack, Tooltip } from "@mui/material";
-import { useContext } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import DeleteSweep from "@mui/icons-material/DeleteSweep";
 import ContentPasteIcon from "@mui/icons-material/ContentPaste";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import { ProjectContext } from "../../shared/context/ProjectContext";
-import { useSnackbar } from "../../shared/context/SnackbarContext";
 import useAnnotationActions from "../../shared/hooks/api/annotation";
 
 const TagPopover = ({ textId, tokenId, entityLabelId }) => {
-  const [state, dispatch] = useContext(ProjectContext);
-  const { deleteLabelAction } = useAnnotationActions();
+  const { deleteLabelAction, applyLabelAction } = useAnnotationActions();
 
   const handleDelete = async ({ applyAll = false }) => {
     await deleteLabelAction({ textId, tokenId, entityLabelId, applyAll });
+  };
+
+  const handleApplyAll = async () => {
+    await applyLabelAction({ textId, tokenId, entityLabelId, applyAll: true });
   };
 
   return (
@@ -22,30 +22,25 @@ const TagPopover = ({ textId, tokenId, entityLabelId }) => {
       spacing={1}
       sx={{ p: "0.25rem 0.5rem 0.25rem 0.5rem" }}
     >
-      <Tooltip title="Apply all is currently unavailable">
-        <span>
-          <IconButton size="small" disabled>
-            <ContentPasteIcon fontSize="inherit" />
-          </IconButton>
-        </span>
+      <Tooltip title="Click to apply this label to all similar tokens">
+        <IconButton size="small" onClick={handleApplyAll}>
+          <ContentPasteIcon fontSize="inherit" />
+        </IconButton>
       </Tooltip>
-      {/* <MenuItem>
-        <ListItemIcon>
-          <CheckCircleOutlineIcon />
-        </ListItemIcon>
-        <ListItemText>Apply</ListItemText>
-      </MenuItem> */}
+      {/* TODO: conditionally render accept one and accept all */}
       <Tooltip title="Click to remove this token's label">
         <IconButton size="small" onClick={handleDelete}>
           <DeleteIcon fontSize="inherit" />
         </IconButton>
       </Tooltip>
-      {/* <MenuItem disabled>
-        <ListItemIcon>
-          <DeleteSweep />
-        </ListItemIcon>
-        <ListItemText>Remove All</ListItemText>
-      </MenuItem> */}
+      <Tooltip title="Click to remove this label from all similar tokens">
+        <IconButton
+          size="small"
+          onClick={() => handleDelete({ applyAll: true })}
+        >
+          <DeleteSweep fontSize="inherit" />
+        </IconButton>
+      </Tooltip>
     </Stack>
   );
 };
