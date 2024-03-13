c839712459fc960a583b20ac5faa438b8ae2e12e
4theKnowledge <tylerbikaun@gmail.com>
2024-03-13 21:08:27 +0800

    initial commit

---
+++ client/src/features/Dashboard/Flags.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import StyledCard from "./StyledCard";
+import FlagEditor from "../../shared/components/FlagEditor";
+import { Alert, AlertTitle, Box } from "@mui/material";
+
+const Flags = ({ loading, data, handleUpdate }) => {
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <StyledCard title="Flags">
+      <Box p="0rem 1rem">
+        <Alert severity="warning">
+          <AlertTitle>Important: Manage Flags Carefully</AlertTitle>
+          This section allows you to create, update, and delete flags for your
+          project. Be aware that deleting a flag will also remove all related
+          annotations from texts. Proceed with caution to avoid unintended loss
+          of data.
+        </Alert>
+      </Box>
+      <Box p={2}>
+        <FlagEditor
+          values={data.details.flags.map((f) => f.name)}
+          updateValue={handleUpdate}
+        />
+      </Box>
+    </StyledCard>
+  );
+};
+
+export default Flags;
