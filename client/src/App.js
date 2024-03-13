1cfe79ef6a954146bb88c758aa460df1bb7b3728
4theKnowledge <tylerbikaun@gmail.com>
2024-03-13 10:34:17 +0800

    formatting

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,6 @@ function App() {
           <AppProvider>
             <ProjectProvider>
               <CustomSnackbar />
-
               <BrowserRouter>
                 <ErrorBoundaryProvider>
                   <Routes>
