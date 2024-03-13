034d6c8344a63b0ab193c6aab8afcbbb8d684af2
4theKnowledge <tylerbikaun@gmail.com>
2024-03-13 21:09:18 +0800

    added flag component and logic. added sticky footer.

--- client/src/features/Dashboard/Dashboard.jsx
+++ client/src/features/Dashboard/Dashboard.jsx
@@ -1,6 +1,15 @@
-import { Alert, Box, CircularProgress, Grid } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { downloadFile } from "../../shared/utils/dashboard";
 import { useSnackbar } from "../../shared/context/SnackbarContext";
 import Schema from "./Schema";
@@ -9,6 +18,8 @@ import Overview from "./Overview";
 import Details from "./Details";
 import Annotators from "./Annotators";
 import useDashboardActions from "../../shared/hooks/api/dashboard";
+import LoadingButton from "@mui/lab/LoadingButton";
+import Flags from "./Flags";
 
 const Dashboard = () => {
   const { projectId } = useParams();
@@ -23,6 +34,7 @@ const Dashboard = () => {
     deleteProjectById,
     updateProjectSchema,
     updateProjectDetail,
+    updateProjectFlags,
   } = useDashboardActions();
 
   useEffect(() => {
@@ -89,6 +101,40 @@ const Dashboard = () => {
     }
   };
 
+  const handleUpdateFlags = async (_, flags) => {
+    // Flags are provided by name, need to find their _id to send to backend to ensure
+    // no conflicts occur.
+    const updateFlags = flags.map((flag) => {
+      if (data.details.flags.includes(flag.name)) {
+        // Flag to be removed
+        return flag;
+      } else {
+        return flag;
+      }
+    });
+
+    // Send to backend
+    const updatedFlags = await updateProjectFlags({
+      projectId,
+      flags: updateFlags,
+    });
+
+    if (updateFlags) {
+      setData((prevState) => ({
+        ...prevState,
+        details: {
+          ...prevState.details,
+          flags: updatedFlags,
+        },
+      }));
+      snackbarDispatch({
+        type: "SHOW",
+        message: "Succesfully updated project flags",
+        severity: "success",
+      });
+    }
+  };
+
   const handleUpdateAnnotators = async () => {
     alert("handleUpdateAnnotators - not implemented yet.");
   };
@@ -107,38 +153,95 @@ const Dashboard = () => {
     );
 
   return (
-    <Grid container spacing={2} mt={8}>
-      <Grid item xs={12}>
-        <Overview loading={loading} data={data} />
+    <Grid
+      container
+      direction="column"
+      sx={{ height: "calc(100vh - 128px)", overflow: "hidden" }}
+    >
+      <Grid
+        container
+        item
+        xs
+        sx={{ overflow: "auto" }}
+        spacing={4}
+        mt={1}
+        mb={1}
+      >
+        <Grid item xs={12}>
+          <Overview loading={loading} data={data} />
+        </Grid>
+        <Grid item xs={12}>
+          <Details
+            loading={loading}
+            data={data}
+            handleUpdate={handleUpdateDetails}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Annotators
+            loading={loading}
+            data={data}
+            handleUpdate={handleUpdateAnnotators}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Schema
+            loading={loading}
+            data={data}
+            handleUpdateSchema={handleUpdateSchema}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Flags
+            loading={loading}
+            data={data}
+            handleUpdate={handleUpdateFlags}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Settings
+            loading={loading}
+            data={data}
+            downloadProject={downloadProject}
+            deleteProject={deleteProject}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Details
-          loading={loading}
-          data={data}
-          handleUpdate={handleUpdateDetails}
-        />
-      </Grid>
-      {/* <Grid item xs={12}>
-        <Annotators
-          loading={loading}
-          data={data}
-          handleUpdate={handleUpdateAnnotators}
-        />
-      </Grid> */}
-      <Grid item xs={12}>
-        <Schema
-          loading={loading}
-          data={data}
-          handleUpdateSchema={handleUpdateSchema}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Settings
-          loading={loading}
-          data={data}
-          downloadProject={downloadProject}
-          deleteProject={deleteProject}
-        />
+
+      {/* STICKY FOOTER */}
+      <Grid item xs="auto">
+        <Paper variant="outlined">
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            p="0.5rem 1rem"
+            spacing={1}
+          >
+            <Typography variant="body2">
+              Reminder: Start annotating by clicking "Annotate"
+            </Typography>
+            {/* Save changes with "Update," or */}
+            <Stack direction="row" spacing={2}>
+              <Button
+                variant="contained"
+                component={Link}
+                to={`/project/${projectId}`}
+              >
+                Annotate
+              </Button>
+              {/* TODO: integrate change detection and unified update into this button from all component */}
+              {/* <LoadingButton
+                variant="contained"
+                // loading={isSubmitting}
+                // onClick={handleSubmit}
+                disabled={true}
+              >
+                Update
+              </LoadingButton> */}
+            </Stack>
+          </Stack>
+        </Paper>
       </Grid>
     </Grid>
   );
