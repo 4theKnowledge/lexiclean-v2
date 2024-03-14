aab4443ec7cfa5a0f8998630036f45d13826d6dd
4theKnowledge <tylerbikaun@gmail.com>
2024-03-14 14:43:28 +0800

    Refactored and added min token width.

--- client/src/shared/utils/token.js
+++ client/src/shared/utils/token.js
@@ -1,4 +1,4 @@
-export const getTokenWidth = (value) => {
-  const width = value.length + 1;
-  return `${width}ch`;
-};
+const minWidth = 2;
+
+export const getTokenWidth = (value) =>
+  `${Math.max(value.length + 1, minWidth)}ch`;
