e1f6e2f0013434ae47248304a98a3bf748da41a2
4theKnowledge <tylerbikaun@gmail.com>
2024-03-14 20:43:15 +0800

    added auth0 hooks and logic

--- client/src/shared/components/Layout/LogoutButton.jsx
+++ client/src/shared/components/Layout/LogoutButton.jsx
@@ -1,36 +1,36 @@
 import { Button, IconButton, Tooltip } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import LoginIcon from "@mui/icons-material/Login";
-import { useAppContext } from "../../context/AppContext";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = ({ drawerOpen = true }) => {
-  const { state: appState, dispatch: appDispatch } = useAppContext();
-
-  const handleDummyUserLogout = () => {
-    appDispatch({ type: "LOGOUT_DUMMY_USER" });
-  };
-
-  const handleDummyUserLogin = () => {
-    appDispatch({ type: "LOGIN_DUMMY_USER" });
-  };
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   if (drawerOpen) {
     return (
       <Button
         variant="outlined"
-        startIcon={appState.user ? <LogoutIcon /> : <LoginIcon />}
-        onClick={appState.user ? handleDummyUserLogout : handleDummyUserLogin}
+        startIcon={isAuthenticated ? <LogoutIcon /> : <LoginIcon />}
+        onClick={
+          isAuthenticated
+            ? () => logout({ returnTo: window.location.origin })
+            : loginWithRedirect
+        }
       >
-        Log{appState.user ? "out" : "in"}
+        Log{isAuthenticated ? "out" : "in"}
       </Button>
     );
   } else {
     return (
-      <Tooltip title={`Click to  ${appState.user ? "logout" : "login"}`}>
+      <Tooltip title={`Click to ${isAuthenticated ? "logout" : "login"}`}>
         <IconButton
-          onClick={appState.user ? handleDummyUserLogout : handleDummyUserLogin}
+          onClick={
+            isAuthenticated
+              ? () => logout({ returnTo: window.location.origin })
+              : loginWithRedirect
+          }
         >
-          {appState.user ? <LogoutIcon /> : <LoginIcon />}
+          {isAuthenticated ? <LogoutIcon /> : <LoginIcon />}
         </IconButton>
       </Tooltip>
     );
