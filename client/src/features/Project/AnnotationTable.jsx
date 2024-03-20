e50eb14270ee8df63e1580c44501ab6f2b59f703
4theKnowledge <tylerbikaun@gmail.com>
2024-03-20 16:46:58 +0800

    updated no results found component

--- client/src/features/Project/AnnotationTable.jsx
+++ client/src/features/Project/AnnotationTable.jsx
@@ -1,8 +1,19 @@
 import { useState, useContext } from "react";
-import { Grid, Stack, Skeleton } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import {
+  Grid,
+  Stack,
+  Skeleton,
+  Box,
+  Typography,
+  Paper,
+  Button,
+} from "@mui/material";
 import { TextContainer } from "./TextContainer";
 import { ProjectContext } from "../../shared/context/ProjectContext";
+import SearchIcon from "@mui/icons-material/Search";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const AnnotationTable = () => {
   const [state, dispatch] = useContext(ProjectContext);
@@ -46,14 +57,7 @@ const AnnotationTable = () => {
   );
 
   return (
-    <Grid
-      item
-      xs={12}
-      // onKeyDown={(e) => handleMarkupKeyDownEvent(e)}
-      tabIndex="-1"
-      sx={{ outline: "none" }}
-      p={2}
-    >
+    <Grid item xs={12} tabIndex="-1" sx={{ outline: "none" }} p={2}>
       {!state.textsLoading &&
         state.texts &&
         Object.keys(state.texts).length > 0 &&
@@ -66,18 +70,7 @@ const AnnotationTable = () => {
           />
         ))}
       {!state.textsLoading && Object.keys(state.texts).length === 0 && (
-        <div
-          style={{
-            marginTop: "25vh",
-            textAlign: "center",
-            fontSize: "2rem",
-            fontWeight: "bold",
-            color: "#607d8b",
-          }}
-        >
-          <SearchIcon sx={{ fontSize: "5rem", textAlign: "center" }} />
-          <p>Sorry, no results were found</p>
-        </div>
+        <NoResultsFound />
       )}
       {state.textsLoading && (
         <Stack
@@ -93,6 +86,61 @@ const AnnotationTable = () => {
         </Stack>
       )}
     </Grid>
+  );
+};
+
+const NoResultsFound = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [state, dispatch] = useContext(ProjectContext);
+
+  const handleRefresh = () => {
+    navigate(location.pathname, { replace: true });
+  };
+
+  const handleShowFilters = () => {
+    dispatch({ type: "SET_VALUE", payload: { showFilterModal: true } });
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        p: 3,
+      }}
+    >
+      <Paper variant="outlined" sx={{ textAlign: "center", p: 3 }}>
+        <SearchIcon sx={{ fontSize: 60, mb: 2 }} color="primary" />
+        <Typography variant="h6" color="textSecondary" gutterBottom>
+          No Results Found
+        </Typography>
+        <Typography sx={{ mb: 2 }}>
+          We couldn't find any matching results. Try adjusting your filters or
+          search criteria.
+        </Typography>
+        <Box display="flex" gap={2} justifyContent="center" alignItems="center">
+          <Button
+            startIcon={<RefreshIcon />}
+            variant="outlined"
+            color="primary"
+            onClick={handleRefresh}
+          >
+            Refresh
+          </Button>
+          <Button
+            startIcon={<FilterAltIcon />}
+            variant="outlined"
+            color="primary"
+            onClick={handleShowFilters}
+          >
+            Filters
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
