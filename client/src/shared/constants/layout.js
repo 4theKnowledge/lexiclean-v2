555532717cd9548763c5d5495a93e710ec240a19
4theKnowledge <tylerbikaun@gmail.com>
2024-03-20 16:43:40 +0800

    added modal style.

--- client/src/shared/constants/layout.js
+++ client/src/shared/constants/layout.js
@@ -1,3 +1,14 @@
 export const DrawerWidth = 360;
 export const ANNOTATION_SIDEBAR_WIDTH = 300;
 export const PRIMARY_SIDEBAR_WIDTH = 300;
+
+export const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  bgcolor: "background.paper",
+  borderRadius: 4,
+  boxShadow: 24,
+};
