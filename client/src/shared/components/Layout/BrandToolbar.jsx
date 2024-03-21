337b6f528a2139c9c832fc409da8bb731031032a
4theKnowledge <tylerbikaun@gmail.com>
2024-03-21 12:16:53 +0800

    Made brand a link to landing.

--- client/src/shared/components/Layout/BrandToolbar.jsx
+++ client/src/shared/components/Layout/BrandToolbar.jsx
@@ -1,9 +1,23 @@
 import { Stack, Typography } from "@mui/material";
 import BubbleChartIcon from "@mui/icons-material/BubbleChart";
+import { useNavigate } from "react-router-dom";
 
 const BrandToolbar = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/"); // Navigate to the landing page
+  };
+
   return (
-    <Stack direction="row" alignItems="center" spacing={2} ml={1}>
+    <Stack
+      direction="row"
+      alignItems="center"
+      spacing={2}
+      ml={1}
+      onClick={handleClick}
+      sx={{ cursor: "pointer" }}
+    >
       <BubbleChartIcon sx={{ color: "primary.main" }} />
       <Typography fontWeight={500} fontSize={20} sx={{ color: "primary.main" }}>
         LexiClean
