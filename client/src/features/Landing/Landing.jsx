7a30d8f1a815c28f9a040832baa60f498bb484fb
4theKnowledge <tylerbikaun@gmail.com>
2024-03-21 15:44:11 +0800

    restyled added footer and features.

--- client/src/features/Landing/Landing.jsx
+++ client/src/features/Landing/Landing.jsx
@@ -9,6 +9,7 @@ import {
   Paper,
   Link as MuiLink,
   Skeleton,
+  useMediaQuery,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -19,10 +20,30 @@ import { getAuthServiceStrategy } from "../../shared/auth/AuthServiceConfig";
 import { useContext, useState } from "react";
 import { ThemeContext } from "../../shared/context/ThemeContext";
 
-const Landing = () => {
-  const useAuthStrategy = getAuthServiceStrategy();
-  const { isAuthenticated, login, logout } = useAuthStrategy();
+const features = [
+  {
+    title: "Elevate & Protect Your Data",
+    content:
+      "Boost your data’s quality and safeguard privacy with LexiClean. Our tool combines advanced normalisation and tagging capabilities to refine your text data meticulously, ensuring precision and protection effortlessly.",
+  },
+  {
+    title: "Collaboration Meets Innovation",
+    content:
+      "Embrace collaborative intelligence with LexiClean. Our platform empowers you to join forces with peers, enhancing data accuracy and unveiling deep insights, all within a shared, innovative workspace.",
+  },
+  {
+    title: "Empowering the Future, Openly",
+    content:
+      "Commitment to open-source is at our core. LexiClean invites you to refine text quality and protect sensitive data with full confidence, leveraging our transparent, community-driven solutions in your own environment.",
+  },
+  {
+    title: "Machine Learning Ready",
+    content:
+      "Streamline your annotation tasks with LexiClean’s OpenAI integration. Access annotated datasets effortlessly, readying your machine learning models for the future, faster and more efficiently.",
+  },
+];
 
+const Landing = () => {
   return (
     <Box
       sx={{
@@ -33,46 +54,7 @@ const Landing = () => {
       }}
     >
       <Container component="main" maxWidth="lg" sx={{ flex: 1 }}>
-        {/* Header */}
-        <Box
-          display="flex"
-          p="1rem 0rem"
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{
-            position: "sticky",
-            top: 0,
-            zIndex: 1100,
-            backgroundColor: "inherit",
-          }}
-        >
-          {/* Brand */}
-          <Stack direction="row" spacing={1} alignItems="center">
-            <BubbleChartIcon sx={{ color: "text.brandText" }} />
-            <Typography fontWeight={600} color="text.brandText" variant="h6">
-              LexiClean
-            </Typography>
-          </Stack>
-          {/* Actions */}
-          <Stack direction="row" spacing={1} alignItems="center">
-            <IconButton color="inherit">
-              <GitHubIcon />
-            </IconButton>
-            <ThemeToggleButton />
-            <Button
-              size="small"
-              onClick={
-                isAuthenticated
-                  ? () => logout({ returnTo: window.location.origin })
-                  : login
-              }
-            >
-              {isAuthenticated ? "Logout" : "Login"}
-            </Button>
-            <ActionButton size={"small"} />
-          </Stack>
-        </Box>
-        {/* Main Content */}
+        <Header />
         <MainContent />
       </Container>
       <Footer />
@@ -102,9 +84,56 @@ const ActionButton = ({ size = "medium" }) => {
   );
 };
 
+const Header = () => {
+  const useAuthStrategy = getAuthServiceStrategy();
+  const { isAuthenticated, login, logout } = useAuthStrategy();
+  return (
+    <Box
+      display="flex"
+      p="1rem 0rem"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{
+        position: "sticky",
+        top: 0,
+        zIndex: 1100,
+        backgroundColor: "inherit",
+      }}
+    >
+      {/* Brand */}
+      <Stack direction="row" spacing={1} alignItems="center">
+        <BubbleChartIcon sx={{ color: "text.brandText" }} />
+        <Typography fontWeight={600} color="text.brandText" variant="h6">
+          LexiClean
+        </Typography>
+      </Stack>
+      {/* Actions */}
+      <Stack direction="row" spacing={1} alignItems="center">
+        {/* <Button size="small">Documentation</Button> */}
+        <IconButton color="inherit">
+          <GitHubIcon />
+        </IconButton>
+        <ThemeToggleButton />
+        <Button
+          size="small"
+          onClick={
+            isAuthenticated
+              ? () => logout({ returnTo: window.location.origin })
+              : login
+          }
+        >
+          {isAuthenticated ? "Logout" : "Login"}
+        </Button>
+        <ActionButton size={"small"} />
+      </Stack>
+    </Box>
+  );
+};
+
 const MainContent = () => {
   const { mode } = useContext(ThemeContext);
   const [imageLoaded, setImageLoaded] = useState(false);
+  const matches = useMediaQuery((theme) => theme.breakpoints.down("md"));
 
   return (
     <Box
@@ -112,56 +141,122 @@ const MainContent = () => {
       justifyContent="center"
       alignItems="center"
       sx={{
-        height: "calc(100vh - 128px)",
-        mt: "-64px",
+        flex: 1,
+        my: { xs: "72px", sm: "64px" },
         overflow: "auto",
       }}
     >
       <Grid container spacing={2} alignItems="center">
-        <Grid item md={4} xs={12}>
-          <Stack direction="column" spacing={4}>
-            <Typography variant="h4" gutterBottom>
-              Transform Your Texts with Precision: Collaborative Multi-Task
-              Lexical Normalisation & Entity Tagging
-            </Typography>
-            <Typography fontSize={16} gutterBottom color="text.secondary">
-              Unlock the full potential of your textual data with LexiClean,
-              designed to elevate the quality of your texts for downstream NLP
-              tasks. Collaborative enhance accuracy, streamline workflows, and
-              achieve superior results in all your NLP projects.
-            </Typography>
-            <Box>
-              <ActionButton />
-            </Box>
-          </Stack>
+        <Grid container item xs={12} spacing={4} direction="row">
+          <Grid item lg={4} md={12} xs={12}>
+            <Stack
+              direction="column"
+              spacing={4}
+              sx={{ textAlign: { xs: "center", md: "center", lg: "left" } }}
+            >
+              <Typography variant="h3" gutterBottom>
+                Unlock the Full Potential of Your NLP Data!
+              </Typography>
+              <Typography fontSize={16} gutterBottom color="text.secondary">
+                Are dirty texts jamming your NLP pipelines? Concerned about
+                sensitive information lurking in your data? Say no more!
+                LexiClean is here to support your lexical normalisation and
+                entity tagging projects, offering a powerful solution to refine
+                and secure your data effortlessly.
+              </Typography>
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                gap={2}
+              >
+                <ActionButton />
+                <Button
+                  variant="outlined"
+                  as={Link}
+                  to={`${process.env.REACT_APP_DOCS_URL}`}
+                  sx={{ textDecoration: "none" }}
+                >
+                  Find Out More
+                </Button>
+              </Box>
+            </Stack>
+          </Grid>
+          <Grid item lg={8} md={12} xs={12}>
+            <Paper sx={{ borderRadius: 2 }} elevation={4}>
+              {!matches && (
+                <>
+                  {!imageLoaded && (
+                    <Skeleton
+                      variant="rectangular"
+                      width="100%"
+                      sx={{ paddingTop: "56.25%" }} // Aspect ratio of 16:9
+                    />
+                  )}
+                  <Box
+                    component="img"
+                    sx={{
+                      display: imageLoaded ? "block" : "none",
+                      maxWidth: "100%",
+                      borderRadius: "inherit",
+                      maxHeight: "100%",
+                      objectFit: "contain",
+                    }}
+                    src={`${process.env.PUBLIC_URL}/static/annotation_interface_${mode}.png`}
+                    alt="Annotation Interface"
+                    onLoad={() => setImageLoaded(true)}
+                  />
+                </>
+              )}
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item md={8} xs={12}>
-          <Paper sx={{ borderRadius: 2, height: "100%" }} elevation={4}>
-            {!imageLoaded && (
-              <Skeleton
-                variant="rectangular"
-                width="100%"
-                sx={{ paddingTop: "56.25%" }} // Aspect ratio of 16:9
-              />
-            )}
-            <Box
-              component="img"
-              sx={{
-                display: imageLoaded ? "block" : "none", // Only display the image once it's loaded
-                maxWidth: "100%",
-                borderRadius: "inherit",
-                maxHeight: "100%",
-                objectFit: "contain",
-              }}
-              src={`${process.env.PUBLIC_URL}/static/annotation_interface_${mode}.png`}
-              alt="Annotation Interface"
-              onLoad={() => setImageLoaded(true)} // Set imageLoaded to true when the image finishes loading
-            />
-          </Paper>
+        <Grid
+          item
+          container
+          md={12}
+          xs={12}
+          mt={4}
+          spacing={4}
+          sx={{ display: "flex", alignItems: "stretch" }}
+        >
+          <Features />
         </Grid>
       </Grid>
     </Box>
   );
+};
+
+const Features = () => {
+  return features.map((feature, index) => (
+    <Grid
+      item
+      xs={12}
+      lg={3}
+      md={6}
+      key={`feature-${index}`}
+      sx={{ display: "flex" }}
+    >
+      <Paper
+        elevation={2}
+        sx={{
+          width: "100%",
+          p: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          borderRadius: 2,
+          textAlign: "center",
+        }}
+        variant="outlined"
+      >
+        <Typography variant="h6" component="h3" gutterBottom>
+          {feature.title}
+        </Typography>
+        <Typography variant="body2">{feature.content}</Typography>
+      </Paper>
+    </Grid>
+  ));
 };
 
 const Footer = () => {
