9a356a0c76b49b2d4714d0676845ed05d40de7c2
4theKnowledge <tylerbikaun@gmail.com>
2024-03-25 09:02:38 +0800

    refactored

--- client/src/features/Create/steps/Preprocessing.jsx
+++ client/src/features/Create/steps/Preprocessing.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import {
   Grid,
   TextField,
@@ -16,10 +16,10 @@ import {
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import { getCorpusMetrics } from "../../../shared/utils/create";
 
 const Preprocessing = ({ values, updateValue }) => {
   const corpus = useMemo(() => Object.values(values["corpus"]), [values]);
-  const [originalCorpusDetails, setOriginalCorpusDetails] = useState({});
   const [previewContent, setPreviewContent] = useState(
     "Upload texts to preview"
   );
@@ -28,85 +28,53 @@ const Preprocessing = ({ values, updateValue }) => {
     vocabSize: 0,
     tokenSize: 0,
   });
+  const [originalCorpusDetails, setOriginalCorpusDetails] = useState({});
+
+  const processCorpusDetails = useCallback((corpusArray) => {
+    return getCorpusMetrics(corpusArray);
+  }, []);
 
   useEffect(() => {
+    if (!corpus.length) return;
+
+    let processedCorpus = corpus.map((doc) =>
+      doc.text.replace(/\s+/g, " ").trim()
+    );
+
+    if (values["preprocessLowerCase"]) {
+      processedCorpus = processedCorpus.map((text) => text.toLowerCase());
+    }
+
+    if (values["preprocessRemoveDuplicates"]) {
+      processedCorpus = [...new Set(processedCorpus)];
+    }
+
+    if (values["preprocessRemoveChars"]) {
+      const charsToRemove = new RegExp(
+        `[${values["preprocessRemoveCharSet"].replace(
+          /[-/\\^$*+?.()|[\]{}]/g,
+          "\\$&"
+        )}]`,
+        "g"
+      );
+      processedCorpus = processedCorpus.map((text) =>
+        text.replace(charsToRemove, "").replace(/\s+/g, " ").trim()
+      );
+    }
+
     /**
-     * Update preview data whenever a text file is uploaded and the pre-processing
+     * Update preview data whenever a text file is uploaded or the pre-processing
      * actions are changed.
      */
-    const processCorpus = () => {
-      if (
-        corpus.length !== 0 &&
-        Object.keys(originalCorpusDetails).length === 0
-      ) {
-        /**
-         * 1. Add original corpus details for user
-         * 2. Remove multiple white space and trim
-         */
+    if (!Object.keys(originalCorpusDetails).length) {
+      setOriginalCorpusDetails(
+        processCorpusDetails(corpus.map((doc) => doc.text))
+      );
+    }
 
-        const originalCorpus = corpus.map((text) =>
-          text.replace(/\s+/g, " ").trim()
-        );
-        setOriginalCorpusDetails({
-          corpusSize: originalCorpus.length,
-          vocabSize: new Set(
-            originalCorpus.map((text) => text.split(" ")).flat()
-          ).size,
-          tokenSize: originalCorpus.map((text) => text.split(" ")).flat()
-            .length,
-        });
-      }
-
-      if (corpus) {
-        // Remove multiple white space and trim
-        let preCorpus = corpus.map((text) => text.replace(/\s+/g, " ").trim());
-
-        if (values["preprocessLowerCase"]) {
-          preCorpus = preCorpus.map((text) => text.toLowerCase());
-        }
-        if (values["preprocessRemoveDuplicates"]) {
-          preCorpus = [...new Set(preCorpus)];
-        }
-        if (values["preprocessRemoveChars"]) {
-          const escapedChars = [
-            "[",
-            "]",
-            "{",
-            "}",
-            "(",
-            ")",
-            "*",
-            "+",
-            "?",
-            "|",
-            "^",
-            "$",
-            ".",
-            "\\",
-          ];
-
-          const regexCharsEscaped = values["preprocessRemoveCharSet"]
-            .split("")
-            .map((char) => (escapedChars.includes(char) ? `\\${char}` : char));
-          const regex = new RegExp("[" + regexCharsEscaped + "]", "g");
-          preCorpus = preCorpus.map((text) => text.replace(regex, " "));
-          // Remove multiple white space and trim
-          preCorpus = preCorpus.map((text) => text.replace(/\s+/g, " ").trim());
-        }
-
-        // Add data uploaded to preview content
-        setPreviewContent(preCorpus.slice(0, 1000).join("\n"));
-
-        setCorpusDetails({
-          corpusSize: preCorpus.length,
-          vocabSize: new Set(preCorpus.map((text) => text.split(" ")).flat())
-            .size,
-          tokenSize: preCorpus.map((text) => text.split(" ")).flat().length,
-        });
-      }
-    };
-    processCorpus();
-  }, [corpus, values]);
+    setPreviewContent(processedCorpus.join("\n"));
+    setCorpusDetails(processCorpusDetails(processedCorpus));
+  }, [corpus, values, processCorpusDetails, originalCorpusDetails]);
 
   const actionAppliedToCorpus =
     values["preprocessLowerCase"] ||
@@ -114,19 +82,44 @@ const Preprocessing = ({ values, updateValue }) => {
     values["preprocessRemoveChars"];
 
   return (
-    <Grid container item xs={12}>
-      <Grid
-        item
-        container
-        xs={12}
-        justifyContent="space-evenly"
-        alignItems="center"
-        component={Paper}
-        variant="outlined"
-        mb={2}
-      >
+    <Grid container item spacing={2}>
+      <Grid item xs={12}>
+        <Metrics
+          corpusDetails={corpusDetails}
+          actionAppliedToCorpus={actionAppliedToCorpus}
+          originalCorpusDetails={originalCorpusDetails}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Controls values={values} updateValue={updateValue} />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Corpus Preview"
+          multiline
+          maxRows={10}
+          value={previewContent}
+          fullWidth
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+const Metrics = ({
+  corpusDetails,
+  actionAppliedToCorpus,
+  originalCorpusDetails,
+}) => {
+  return (
+    <Paper variant="outlined" sx={{ p: 2 }}>
+      <Box display="flex" alignItems="center" justifyContent="space-around">
         {Object.keys(corpusDetails).map((key) => (
-          <Grid item xs={3} p={2} key={`grid-item-${key}`}>
+          <Grid item xs={3} key={`grid-item-${key}`}>
             <Stack direction="column" alignItems="center">
               <MetricWithDiff
                 actionAppliedToCorpus={actionAppliedToCorpus}
@@ -144,98 +137,85 @@ const Preprocessing = ({ values, updateValue }) => {
             </Stack>
           </Grid>
         ))}
-      </Grid>
-      <Grid container item xs={12}>
-        <Grid item xs={12} component={Paper} variant="outlined" mb={2}>
-          <FormControl sx={{ m: 1 }} component="fieldset">
-            <FormLabel component="legend">Preprocessing Actions</FormLabel>
-            <FormGroup style={{ display: "flex", flexDirection: "row" }}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={values["preprocessLowerCase"]}
-                    onChange={(e) => {
-                      updateValue({
-                        key: "preprocessLowerCase",
-                        value: e.target.checked,
-                      });
-                    }}
-                    name="remove-casing"
-                    title="Removes casing from characters. This can reduce annotation effort."
-                  />
-                }
-                label="Lower Case"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={values["preprocessRemoveChars"]}
-                    onChange={(e) => {
-                      updateValue({
-                        key: "preprocessRemoveChars",
-                        value: e.target.checked,
-                      });
-                    }}
-                    title="Removes special characters from corpus. This can reduce annotation effort."
-                    name="remove-chars"
-                  />
-                }
-                label="Remove Characters"
-              />
+      </Box>
+    </Paper>
+  );
+};
 
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={values["preprocessRemoveDuplicates"]}
-                    onChange={(e) => {
-                      updateValue({
-                        key: "preprocessRemoveDuplicates",
-                        value: e.target.checked,
-                      });
-                    }}
-                    title="Removes duplicate documents from your corpus. This can reduce annotation effort."
-                    name="remove-duplicates"
-                  />
-                }
-                label="Remove Duplicates"
-              />
-              <TextField
-                id="remove-char-text-field"
-                label="Characters To Remove"
-                variant="standard"
-                size="small"
-                autoComplete="off"
-                value={values["preprocessRemoveCharSet"]}
+const Controls = ({ values, updateValue }) => {
+  return (
+    <Paper variant="outlined" sx={{ p: 2 }}>
+      <FormControl sx={{ m: 1 }} component="fieldset">
+        <FormLabel component="legend">Preprocessing Actions</FormLabel>
+        <FormGroup style={{ display: "flex", flexDirection: "row" }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={values["preprocessLowerCase"]}
                 onChange={(e) => {
                   updateValue({
-                    key: "preprocessRemoveCharSet",
-                    value: e.target.value,
+                    key: "preprocessLowerCase",
+                    value: e.target.checked,
                   });
                 }}
-                disabled={!values["preprocessRemoveChars"]}
-                placeholder={values["preprocessRemoveCharSet"]}
+                name="remove-casing"
+                title="Removes casing from characters. This can reduce annotation effort."
               />
-            </FormGroup>
-          </FormControl>
-        </Grid>
-      </Grid>
-
-      <Grid item container xs={12} spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Corpus Preview"
-            multiline
-            maxRows={10}
-            value={previewContent}
-            fullWidth
-            InputProps={{
-              readOnly: true,
-            }}
+            }
+            label="Lower Case"
           />
-        </Grid>
-      </Grid>
-    </Grid>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={values["preprocessRemoveChars"]}
+                onChange={(e) => {
+                  updateValue({
+                    key: "preprocessRemoveChars",
+                    value: e.target.checked,
+                  });
+                }}
+                title="Removes special characters from corpus. This can reduce annotation effort."
+                name="remove-chars"
+              />
+            }
+            label="Remove Characters"
+          />
+
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={values["preprocessRemoveDuplicates"]}
+                onChange={(e) => {
+                  updateValue({
+                    key: "preprocessRemoveDuplicates",
+                    value: e.target.checked,
+                  });
+                }}
+                title="Removes duplicate documents from your corpus. This can reduce annotation effort."
+                name="remove-duplicates"
+              />
+            }
+            label="Remove Duplicates"
+          />
+          <TextField
+            id="remove-char-text-field"
+            label="Characters To Remove"
+            variant="standard"
+            size="small"
+            autoComplete="off"
+            value={values["preprocessRemoveCharSet"]}
+            onChange={(e) => {
+              updateValue({
+                key: "preprocessRemoveCharSet",
+                value: e.target.value,
+              });
+            }}
+            disabled={!values["preprocessRemoveChars"]}
+            placeholder={values["preprocessRemoveCharSet"]}
+          />
+        </FormGroup>
+      </FormControl>
+    </Paper>
   );
 };
 
@@ -250,6 +230,8 @@ const MetricWithDiff = ({
   corpusDetails,
   name,
 }) => {
+  console.log("name: ", name);
+
   const comparisonResult = useMemo(() => {
     const originalValue = originalCorpusDetails[name];
     const newValue = corpusDetails[name];
