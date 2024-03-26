44402b2ffdd9dee35099cc7c5bdc6f129b1f5e44
4theKnowledge <tylerbikaun@gmail.com>
2024-03-26 15:57:50 +0800

    added indicator if user is in local mode.

--- client/src/shared/components/Layout/BrandToolbar.jsx
+++ client/src/shared/components/Layout/BrandToolbar.jsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Chip, Stack, Tooltip, Typography } from "@mui/material";
 import BubbleChartIcon from "@mui/icons-material/BubbleChart";
 import { useNavigate } from "react-router-dom";
 
@@ -6,22 +6,40 @@ const BrandToolbar = () => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate("/"); // Navigate to the landing page
+    navigate("/");
   };
 
   return (
-    <Stack
-      direction="row"
-      alignItems="center"
-      spacing={2}
-      ml={1}
-      onClick={handleClick}
-      sx={{ cursor: "pointer" }}
-    >
-      <BubbleChartIcon sx={{ color: "primary.main" }} />
-      <Typography fontWeight={500} fontSize={20} sx={{ color: "primary.main" }}>
-        LexiClean
-      </Typography>
+    <Stack direction="row" alignItems="center" spacing={1} ml={1}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={2}
+        sx={{ cursor: "pointer" }}
+        onClick={handleClick}
+      >
+        <BubbleChartIcon sx={{ color: "primary.main" }} />
+        <Typography
+          fontWeight={500}
+          fontSize={20}
+          sx={{ color: "primary.main" }}
+        >
+          LexiClean
+        </Typography>
+      </Stack>
+      {process.env.REACT_APP_AUTH_STRATEGY.toLowerCase() === "dummy" && (
+        <Tooltip
+          title="You are running LexiClean in local mode"
+          arrow
+          placement="bottom"
+        >
+          <Chip
+            label="Local"
+            size="small"
+            sx={{ fontSize: 10, pointer: "none", cursor: "help" }}
+          />
+        </Tooltip>
+      )}
     </Stack>
   );
 };
