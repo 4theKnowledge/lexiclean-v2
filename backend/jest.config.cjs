1d2c7c3369285e1e662e3694c7d8bb9d272f4ee4
4theKnowledge <tylerbikaun@gmail.com>
2024-06-03 17:53:15 +0800

    updated backend tests and ci

---
+++ backend/jest.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  testEnvironment: "node",
+  transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$"],
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  globalTeardown: "./tests/teardown.js",
+  // Coverage configuration
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+};
